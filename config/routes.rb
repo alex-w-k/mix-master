b1d542c456db9f26c8f080e5bf9da6863dcc44e3
Alex Koch <alex549us3@gmail.com>
2017-05-18 09:06:45 -0600

    i4 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :artists do 
+    resources :songs, only: [:new, :create]
+  end
+
+  resources :songs, only: [:show]
+  resources :playlists, only: [:index, :new, :create, :show, :edit, :update]
 end
